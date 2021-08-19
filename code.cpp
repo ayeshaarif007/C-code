128222163b4177c17d222b53906bd8597e3a5dd0
Ayesha Arif <aaish007@yahoo.com>
2021-08-19 23:22:19 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main()
 {
   const int MAX_SIZE=30;
-  int marks[MAX_SIZE];
+  int marks[MAX_SIZE];  //Partially filled array.
   int marks, numOfStu, sum=0;
   cout<<"Enter marks of students: ";
   cin>>marks;
-  while(marks!=-99 && numOfStu<MAX_SIZE)
+  cout<<"Enter -99 to stop input.\n";
+  while(marks!=-99 && numOfStu<MAX_SIZE)  //Sentinel-controlled loop.
   {
     marks[numOfStu]=marks;
     numOfStu++;
@@ -15,7 +16,8 @@ int main()
     cin>>marks;
   }
   for(int i=0; i<numOfStu; i++)
-    sum=sum+marks[i];
+    sum=sum+marks[i];   //Calculating the sum of values stored in the array.
   cout<<"The Sum of marks of students is: " <<sum;
+  
   return 0;
 }
